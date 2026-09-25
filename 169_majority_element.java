041f8995cf9df15436d13e6a02fce90a360a6bf7
Ulvi <rahimli.ulvi2004@gmail.com>
2026-09-25 15:22:08 +0400

    987623498

---
+++ 169_majority_element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int result = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                result = num;
+            }
+
+            if (num == result) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return result;
+    }
+}
